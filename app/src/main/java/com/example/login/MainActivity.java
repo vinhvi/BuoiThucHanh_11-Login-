4140e8a96ab7b97bff4dbd41a74212d5683f682e
Vinh <vithevinh123@gmail.com>
2022-04-18 16:10:56 +0700

    finish 2

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.view4);
+        setContentView(R.layout.view3);
     }
 }
